3e870ea1d470d1cab9ec1d8192f2bcc390a3cf90
Azhar730 <azharmahmud730@gmail.com>
2025-07-19 19:42:24 +0600

    some editing project

--- src/modules/ClassSchedule/schedule.route.ts
+++ src/modules/ClassSchedule/schedule.route.ts
@@ -5,7 +5,7 @@ import auth from '../../middlewares/auth';
 const router = Router();
 
 router.post('/create-schedule',auth('admin'), ScheduleControllers.createSchedule);
-router.get('/',auth('admin'), ScheduleControllers.getAllSchedules);
-router.get('/:id',auth('admin'), ScheduleControllers.getSingleSchedule);
+router.get('/', ScheduleControllers.getAllSchedules);
+router.get('/:id', ScheduleControllers.getSingleSchedule);
 
 export const ScheduleRoutes = router;
